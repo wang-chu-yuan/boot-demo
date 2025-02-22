507788fb99f6fa501051102ce06354abe76fab67
Jack_Wang <wcyis1998@gmail.com>
2025-02-22 20:11:17 +0800

    提交测试1

--- src/main/java/com/tutorial/boot_demo/BootDemoApplication.java
+++ src/main/java/com/tutorial/boot_demo/BootDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BootDemoApplication {
-
+// 提交测试
 	public static void main(String[] args) {
 		SpringApplication.run(BootDemoApplication.class, args);
 	}
